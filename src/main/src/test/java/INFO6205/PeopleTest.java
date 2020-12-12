1034598d013533dbad9c7b923276d732f5594dc5
Vennela <karaddi.v@husky.neu.edu>
2020-12-11 23:13:39 -0500

    final project

---
+++ src/main/src/test/java/INFO6205/PeopleTest.java
@@ -0,0 +1,31 @@
+package INFO6205;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeopleTest {
+    @Mock
+    public Place home;
+    @Mock
+    public Place publicPlace;
+    @Mock
+    People mockPeople;
+
+    @Test
+    public void VerifyAgeisLessThan100(){
+       People people = new People();
+        int a =people.returnAge();
+        assertTrue(a<100);
+
+
+    }
+    @Test
+    public void verifyPeopleObjectIsNotNull(){
+        People people = new People();
+        assertNotNull(people);
+
+    }
+
+}
---
+++ src/main/src/test/java/INFO6205/Place2Test.java
@@ -0,0 +1,28 @@
+package INFO6205;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Place2Test {
+    @Mock
+    Position position;
+    List<Position> positions = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+
+        positions.add(position);
+    }
+
+    @Test
+    public void VerifyPlaceObjectIsCreatedWhenListOfPositionIsSupplied(){
+        Place place =new Place1(positions);
+        assertNotNull(place);
+    }
+}
